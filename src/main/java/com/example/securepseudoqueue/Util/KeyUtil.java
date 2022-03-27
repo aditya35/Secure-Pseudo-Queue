0cbe0300945ab7baf11f87b742c0d625d5c01ec3
Aditya Sehkhar <adityashekhar35@gmail.com>
2022-03-27 23:54:08 +0530

    Encryption key setup

---
+++ src/main/java/com/example/securepseudoqueue/Util/KeyUtil.java
@@ -0,0 +1,27 @@
+package com.example.securepseudoqueue.Util;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+public class KeyUtil {
+
+    public static final String PASSWORD = "testPassword";
+    public static final String SALT = "testSalt";
+    public static final String CIPHER_TYPE = "AES";
+
+
+    public static SecretKey getKeyFromPassword(String password, String salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+        SecretKey secret = new SecretKeySpec(factory.generateSecret(spec)
+                .getEncoded(), "AES");
+        return secret;
+    }
+}
