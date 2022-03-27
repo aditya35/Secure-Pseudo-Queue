9bc44fbc37426dca94daa837273aacfb85962af7
Aditya Sehkhar <adityashekhar35@gmail.com>
2022-03-28 00:30:44 +0530

    update heroku endpoint

--- src/main/java/com/example/securepseudoqueue/Constant/UrlConstant.java
+++ src/main/java/com/example/securepseudoqueue/Constant/UrlConstant.java
@@ -2,5 +2,5 @@ package com.example.securepseudoqueue.Constant;
 
 public class UrlConstant {
 
-    public static String RECIEVE_URL = "http://localhost:8080/api/v1/receiver";
+    public static String RECIEVE_URL = "https://secure-psedu-queue.herokuapp.com/api/v1/receiver";
 }
